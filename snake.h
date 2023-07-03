baee768c3ad360781c53847ec0833bd81245db82
Ray <212245086@qq.com>
2023-07-03 16:29:50 +0800

    完善 Snake 类声明

--- snake.h
+++ snake.h
@@ -2,25 +2,36 @@
 #define SNAKE_H
 
 #include "item.h"
+#include "field.h"
 #include <vector>
 using namespace std;
 
 typedef pair<int, int> Loc;
+enum Direction {up=0, down, left, right};
 
 class Snake
 {
 public:
-    Snake(vector<Loc>, int length);
+    Snake(vector<Loc>, int length, int MAX_health, Direction direction, Grid itemMap);
+    // 给定头部坐标, 按长度默认初始化一条朝向指定方向的蛇
+    Snake(Loc head, int length, int MAX_health, Direction direction, Grid itemMap);
 
-    void changeDirection(string);
+    void initialize();
+
+    void changeDirection(Direction);
+    Loc nextLoc();
     void move();    // 前进, 检查碰到的物体并 Item.action(this) , 更新身体坐标以及其他参数
     Item* hitItem();
 
     bool hitSelf();
-    bool hitOtherSnake(vector<Snake>);  // 在 Game 中可以把 field.snakes[1:] 传进来
+    bool hitOtherSnake(vector<Snake*>);  // 在 Game 中可以把 field.snakes[1:] 传进来
 
+    void addLength(int);   // 在当前尾部添加给定个长度, 并更新 length
+    void decreaseHealth(int);
     void death();   // 死亡程序, 在 revival > 0 的情况下重新在地图中心初始化, 否则返回死亡
 
+    int MAX_health;
+    Grid itemMap;
 
     int length;
     vector<Loc> body;   // body[0] 是头部
@@ -33,7 +44,7 @@ public:
     */
     int speed = 3;
     int cycle_recorder = 1; //  cycle_recorder 每增加 (6 - speed) 该蛇进行一轮操作
-    int direction;  // "up" "down" "left" "right"
+    Direction direction;  // "up" "down" "left" "right"
 
     // 特殊能力倒计时, 随全局时钟变化
     int magnetic = 0;   
