4a1deda8b62de5bac7f0c5c7cd1e7574d9abcec6
Han Xiaoshen <84143195+hanleo001@users.noreply.github.com>
2023-07-04 09:53:47 +0800

    add a FoodGready snake

---
+++ AISnake.h
@@ -0,0 +1,28 @@
+//
+// Created by lenovo on 2023/7/3.
+//
+
+#ifndef SNAKE_SRC_FULL_AISNAKE_H
+#define SNAKE_SRC_FULL_AISNAKE_H
+#include "snake.h"
+
+class AISnake:public Snake {
+public:
+    virtual Direction act(Field& state) = 0;
+
+};
+
+
+class GreedyFood:public AISnake{
+private:
+    pair<int, int> getNearestFood(const Field& state);
+public:
+    virtual Direction act(Field& state);
+};
+
+#endif //SNAKE_SRC_FULL_AISNAKE_H
+
+
+class Hardhitted:public AISnake{
+
+};
